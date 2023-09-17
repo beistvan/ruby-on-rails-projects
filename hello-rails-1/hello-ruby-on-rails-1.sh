c1b9add66a9a2967b51f83523469fd3a25061ef0
Beregszászi István <beistvan@gmail.com>
2023-09-17 14:35:29 +0200

    Add Contoller

--- hello-rails-1/hello-ruby-on-rails-1.sh
+++ hello-rails-1/hello-ruby-on-rails-1.sh
@@ -3,3 +3,11 @@ rails new MySite
 cd MySite
 bundle install
 rails server
+
+# controller
+rails generate controller Pages
+# app/controllers/pages_controller.rb
+# class PagesController < ApplicationController
+#  def home
+#  end
+# end
