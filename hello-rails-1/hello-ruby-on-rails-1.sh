824827022f2dea77bb5994aeddcfdce6cb638557
Beregszászi István <beistvan@gmail.com>
2023-09-17 14:45:06 +0200

    Add View

--- hello-rails-1/hello-ruby-on-rails-1.sh
+++ hello-rails-1/hello-ruby-on-rails-1.sh
@@ -16,3 +16,16 @@ rails generate controller Pages
 # config/routes.rb
 # get 'welcome', to: 'pages#home'
 
+# views
+# app/views/pages/home.html.erb
+# <div class="main">
+#  <div class="container">
+#    <h1>Hello my name is István</h1>
+#    <p>I make Rails apps.</p>
+#  </div>
+# </div>
+
+# styles
+# app/assets/stylesheets/pages.css.scss
+# image for background bg-static.jpg - put ot he app/assets/images folder
+# http://localhost:4001/welcome
